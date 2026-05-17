634c9e0800726afbd02887595cfc2de83315a1d0
Daniil <dan.nikitin.018@gmail.com>
2026-05-17 19:28:11 +0300

    Init Commit

---
+++ shared/src/commonMain/kotlin/com/example/gamevault/App.kt
@@ -0,0 +1,13 @@
+package com.example.gamevault
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.*
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun App() {
+    MaterialTheme {
+
+    }
+}
